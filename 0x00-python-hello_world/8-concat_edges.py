ba61b5a1f1a7c63ed54154ebca44249554fde4dc
SandraHoyos <slhoyosr@gmail.com>
2021-09-06 23:34:17 -0500

    Shell script that runs Python code and prints a str variable with format

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+cadena = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+cadena = cadena[39:66] + cadena[106:112] + cadena[:6]
+print(str)
