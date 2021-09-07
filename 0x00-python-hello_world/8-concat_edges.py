1daa5b8c9953af2fa10e0cff2c74d6486d04466d
SandraHoyos <slhoyosr@gmail.com>
2021-09-06 23:36:12 -0500

    Shell script that runs Python code and prints a str variable with formatf

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-cadena = "Python is an interpreted, interactive, object-oriented programming\
+str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-cadena = cadena[39:66] + cadena[106:112] + cadena[:6]
+str = str[39:66] + str[106:112] + str[:6]
 print(str)
