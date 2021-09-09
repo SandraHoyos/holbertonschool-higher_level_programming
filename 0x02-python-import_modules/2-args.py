2af4c718c0a9406494ff3d27d04038582b4f7d9d
SandraHoyos <slhoyosr@gmail.com>
2021-09-08 23:31:43 -0500

    first commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-if _name_ == '_main_':
+if __name__ == '__main__':
     import sys
     arg = len(sys.argv)
     if arg == 1:
         print("0 arguments.")
     elif arg == 2:
         print("{} argument:".format(arg - 1))
+ """
     else:
         print("{}arguments:".format(arg - 1))
+""" 
     for i in range(1, arg):
         print("{}: {}".format(i, sys.argv[i]))
