bcef7011f8c876267f840926409f42293e67a22f
SandraHoyos <slhoyosr@gmail.com>
2021-09-08 23:47:04 -0500

    First commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
+##!/usr/bin/python3
 if __name__ == '__main__':
     import sys
     arg = len(sys.argv)
     suma = 0
     for i in range(1, arg):
-        suma = suma + int(sys.argv[i])
+        suma += int(sys.argv[i])
     print("{}".format(suma))
