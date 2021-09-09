4df820dddc34a4d925e3f23b25abe1ecc9cd61be
SandraHoyos <slhoyosr@gmail.com>
2021-09-08 20:41:56 -0500

    calculator

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from calculator_1 import *
+
+if _name_ == ' _main_ ':
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, add(a, b)))
+    print("{} * {} = {}".format(a, b, add(a, b)))
+    print("{} / {} = {}".format(a, b, add(a, b)))
