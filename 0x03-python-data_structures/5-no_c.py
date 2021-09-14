602a10bd22330f28e7cae9256139f5f48d384768
SandraHoyos <slhoyosr@gmail.com>
2021-09-13 21:50:14 -0500

    function that removes all characters

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    cadena = ''
+    for element in my_string:
+        if element != "c" and element != "c":
+            cadena += element
+            return cadena
