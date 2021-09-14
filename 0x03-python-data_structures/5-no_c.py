1bad1bc1fb4f80ce480834014945e3b84cb839b1
SandraHoyos <slhoyosr@gmail.com>
2021-09-13 22:58:44 -0500

    function that removes all characters

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    cadena = ''
-    for element in my_string:
-        if element != "c" and element != "c":
-            cadena += element
-            return cadena
+    return ''.join(c for c in my_string if c not in 'cC')
