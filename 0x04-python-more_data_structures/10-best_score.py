39cc612fcc7fe79f0d83d3b593c9b0d197bbacaa
SandraHoyos <slhoyosr@gmail.com>
2021-09-16 23:27:50 -0500

    Higher Score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if a_dictionary is None:
         return None
-    maxi = max(a_dictionary.values(), default=None)
-    for key, value in a_dictionary.items():
-        if value == maxi:
+    else:
+        maxi = max(a_dictionary.values(), default=None)
+        for key, value in a_dictionary.items():
+            if value == maxi:
             return key
     
