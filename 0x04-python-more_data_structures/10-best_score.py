5dbb47e9b264ff52c3d4327b1d57bb0bdb5755ba
SandraHoyos <slhoyosr@gmail.com>
2021-09-16 22:52:13 -0500

    multiply by 2

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python
 def best_score(a_dictionary):
-    if is not a_dictionary:
-        return None
-    return max(a_dictionary.values[], default = None)
+    return max(a_dictionary.values(), default=None)
