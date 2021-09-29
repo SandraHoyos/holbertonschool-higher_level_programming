ed220e11caba4412d0b5dfea522b278c5f9e3f6f
SandraHoyos <slhoyosr@gmail.com>
2021-09-28 21:33:48 -0500

    fix

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,26 +1,57 @@
 #!/usr/bin/python3
+"""
+Module 4-square
+Defines class Square with private size and public area
+Can access and update size
+"""
 
-"class square defines a square"
 
 class Square:
     """
-    Class square
+    class Square definition
+    Args:
+        size (int): size of a side in square
+    Functions:
+        __init__(self, size)
+        size(self)
+        size(self, value)
+        area(self)
     """
+
     def __init__(self, size=0):
         """
-        Inizialition of variables
-        Arg self identificador
-        size tamañe of square
+        Initializes square
+        Attributes:
+            size (int): defaults to 0 if none; don't use __size to call setter
         """
-        if type(size) is not int:
+        self.size = size
+
+    @property
+    def size(self):
+        """"
+        Getter
+        Return: size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter
+        Args:
+            value: sets size to value, if int and >= 0
+        """
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = value
 
     def area(self):
         """
-        Returns area the square
+        Calculates area of square
+        Returns:
+            area
         """
-        return self.__size * self.__size
+        return (self.__size)**2
