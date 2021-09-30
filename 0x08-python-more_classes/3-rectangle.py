457281369dc8ad350bbb3a39f722505cef49ab3b
SandraHoyos <slhoyosr@gmail.com>
2021-09-29 22:12:15 -0500

    String

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,4 +1,33 @@
-   return self.__height
+#!/usr/bin/python3
+
+
+class Rectangle:
+    """Defines the implementation of a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Property retriever, for retreiving"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Property setter, for setting"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Property retriever, for retreiving
+        the rectangle height
+        """
+        return self.__height
 
     @height.setter
     def height(self, value):
