aef05a97ea40c69987cad6d1d9830dac7e4da37e
SandraHoyos <slhoyosr@gmail.com>
2021-10-12 14:13:46 -0500

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,14 @@
-!#
+#!/usr/bin/python3
+"""reads from a file and prints"""
+
+
+
+def read_file(filename=""):
+    """Reads from filename and prints
+    its contents to stdout
+    filename is name of the file"""
+
+
+    with open(filename) as f:
+        read_text = f.read()
+        print(read_text, end="")
