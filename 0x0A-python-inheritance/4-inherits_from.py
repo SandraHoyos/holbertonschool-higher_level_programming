e1198b2e9eedf5d35bd5ad3bcf5d2cb6cfb593b7
SandraHoyos <slhoyosr@gmail.com>
2021-10-11 23:26:48 -0500

    fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-""" Module base geometry
+"""
+module only sub class of
 """
 
 
-class BaseGeometry:
-    """ empty class"""
-    pass
+def inherits_from(obj, a_class):
+    """returns True if the object is an instance of a class
+       that inherited (directly or indirectly) from the specified class;
+       otherwise False.
+    """
+    if type(obj) != a_class:
+        return issubclass(type(obj), a_class)
+    else:
+        return False
