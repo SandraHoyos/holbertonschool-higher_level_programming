1480bb1f290c2da63831f0d60d051509d200dfcc
SandraHoyos <slhoyosr@gmail.com>
2021-10-12 23:36:02 -0500

    pep8

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ defines"""
-
+import json
 
 def save_to_json_file(my_obj, filename):
     """save to json file
@@ -8,6 +8,5 @@ def save_to_json_file(my_obj, filename):
        my_obj
        filename
     """
-    import json
     with open(filename, 'w') as f:
         f.write(json.dumps(my_obj)
