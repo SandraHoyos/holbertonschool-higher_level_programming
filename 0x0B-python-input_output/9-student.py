f1699481aa1a2ab3456919da885ea769853f83d9
SandraHoyos <slhoyosr@gmail.com>
2021-10-13 11:43:22 -0500

    studet class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""creates a student"""
+
+class student:
+    """class that defines a student
+    public attributes:
+    first_name
+    last_name
+    age
+    public method to:json()"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializes the student instance
+        arg:
+        first_name: the first name of the student
+        last_name: the last name the student
+        age: the age of the student"""
+
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self:age = age
+
+    def to_json(self):
+        """gets a dictionary representation of the student"""
+
+        return self.__dict__
