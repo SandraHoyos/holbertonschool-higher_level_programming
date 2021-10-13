06978030c69613bb07a9ff86f7f30c03b35a3d95
SandraHoyos <slhoyosr@gmail.com>
2021-10-12 23:32:01 -0500

    pep8

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-import json
-"""
-This file contains a function that
-returns an object represented by a
-JSON string
-"""
+""" defines a json object"""
 
 
 def from_json_string(my_str):
+    """load to json from str
+    Args
+       my_str
     """
-    function to return object
-    represented by a JSON string
-    """
-    return (json.loads(my_str))
+    import json
+    return(json.loads(my_str))
