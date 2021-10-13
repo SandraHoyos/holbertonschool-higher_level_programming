d0585c93331109979d3484d9de33ac2ac70f0b53
SandraHoyos <slhoyosr@gmail.com>
2021-10-13 11:47:01 -0500

    student class

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """creates a student"""
 
+
 class student:
     """class that defines a student
     public attributes:
@@ -16,10 +17,9 @@ class student:
         last_name: the last name the student
         age: the age of the student"""
 
-
         self.first_name = first_name
         self.last_name = last_name
-        self:age = age
+        self.age = age
 
     def to_json(self):
         """gets a dictionary representation of the student"""
