f361025da77fcfdac6cf18d40c40355e8d1098e2
SandraHoyos <slhoyosr@gmail.com>
2021-10-13 21:25:00 -0500

    fix

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
-"""creates a student"""
+"""
+Contains the clas "Student"
+"""
 
 
-class student:
-    """class that defines a student
-    public attributes:
-    first_name
-    last_name
-    age
-    public method to:json()"""
-
+class Student:
+    """Representation of a student"""
     def __init__(self, first_name, last_name, age):
-        """initializes the student instance
-        arg:
-        first_name: the first name of the student
-        last_name: the last name the student
-        age: the age of the student"""
-
+        """Initializes the student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """gets a dictionary representation of the student"""
-
+        """returns a dictionary representation of a Student instance"""
         return self.__dict__
