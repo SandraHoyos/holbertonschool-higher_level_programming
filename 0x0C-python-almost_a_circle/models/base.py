bfb9b6f65f0095c4c2562540aa0af8c7f702e3ff
SandraHoyos <slhoyosr@gmail.com>
2021-10-18 23:00:14 -0500

    fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,31 +5,33 @@ import json
 
 
 class Base:
+    """Represents the base class
+    Observation:
+        The class is used to manage id attribute
+        in all your future classes and to avoid duplicating
+        the same code (by extension, same bugs)
     """
-    This class has a private attribute __nb_objects = 0
-    a class constructor definition to check for id
-    """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """
-        Instantiation of class which checks for id
+        """Instantiates the id class with
+        Args:
+            id(int): the class id
         """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-        
+
     @staticmethod
     def to_json_string(list_dictionaries):
-
         """Returns the JSON string representation
         of list_dictionaries
         Args:
             list_dictionaries(list): list of dictionary
         """
-
         if list_dictionaries:
             return json.dumps(list_dictionaries)
         return "[]"
@@ -55,14 +57,14 @@ class Base:
         """Returns the list of the JSON string
         representation json_string
         Args:
-            list_dictionaries(str): a string representing a list
+            json_string(str): a string representing a list
             of dictionaries
         """
-        if list_dictionaries is None:
+        if json_string is None:
             return []
-        if list_dictionaries == [] or not isinstance(json_string, str):
+        if json_string == [] or not isinstance(json_string, str):
             return []
-        return json.dumps(json_string)
+        return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
@@ -92,40 +94,5 @@ class Base:
                 s = cls.from_json_string(file.read())
             for i in s:
                 list_instance.append(cls.create(**i))
-            return list_instance
-        return []
-
-    @classmethod
-    def save_to_file_csv(cls, list_objs):
-        """A  class method that serializes and
-        deserializes in CSV
-        """
-        if (list_objs is None or not isinstance(list_objs, list)
-                or not all(isinstance(j, Base) for j in list_objs)):
-            with open(cls.__name__ + ".csv", "w") as file:
-                file.write("[]")
-        if list_objs and any(isinstance(j, Base) for j in list_objs):
-            dict_data = [i.to_dictionary() for i in list_objs]
-            if cls.__name__ == "Rectangle":
-                csv_columns = ["id", "width", "height", "x", "y"]
-            else:
-                csv_columns = ["id", "size", "x", "y"]
-            with open(cls.__name__ + ".csv", "w") as file:
-                writer = csv.DictWriter(file, fieldnames=csv_columns)
-                writer.writeheader()
-                for data in dict_data:
-                    writer.writerow(data)
-
-    @classmethod
-    def load_from_file_csv(cls):
-        """Loads from csv file"""
-        list_instance = []
-        name = cls.__name__ + ".csv"
-        if os.path.isfile(name):
-            with open(name, "r") as file:
-                reader = csv.DictReader(file)
-                for row in reader:
-                    d = {key: int(value) for key, value in row.items()}
-                    list_instance.append(cls.create(**d))
             return list_instance
         return []
