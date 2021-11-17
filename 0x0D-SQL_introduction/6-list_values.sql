e88477c8d08c1d92547347a4cff0a488278f649a
SandraHoyos <slhoyosr@gmail.com>
2021-11-16 20:26:20 -0500

    list values

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,3 +1,5 @@
 -- script that lists all rows of the table first_table from the database hbtn_0c_0
 -- all fields should be printed
-SHOW CREATE TABLE first_table;
+-- the database name will be passed as argument of the mysql command
+
+SELECT * FROM first_table;
