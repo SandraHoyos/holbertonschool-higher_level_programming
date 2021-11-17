53a1c01f0bd0d289ebf19d5a3d0ac9f66242fb00
SandraHoyos <slhoyosr@gmail.com>
2021-11-16 22:19:06 -0500

    top score

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,4 @@
 -- lists all records of the table second table of the database hbtn_0c_0
-
+-- Results should display both the score and the name (in this order)
+-- Records should be ordered by score (top first)
 SELECT SCORE, NAME FROM second_table ORDER BY score DESC;
