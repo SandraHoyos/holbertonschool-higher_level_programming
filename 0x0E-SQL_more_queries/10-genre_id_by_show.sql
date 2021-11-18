d49a1f4b752919652b6dbf90b6e66f872844a885
SandraHoyos <slhoyosr@gmail.com>
2021-11-18 17:02:46 -0500

    genre id by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -5,5 +5,5 @@
 SELECT tv_show.title, tv_show_genres.genre_id
   FROM tv_shows
 INNER JOIN tv_show_genres
-        ON tv_show.id = tv_show_genres.show_id
+        ON tv_shows.id = tv_show_genres.show_id
   ORDER Y tv_shows.title, tv_show_genres.genre_id ASC;
