49c643092685974c87ea12538ecddf1e9cf797e8
SandraHoyos <slhoyosr@gmail.com>
2021-11-17 21:53:13 -0500

    create read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,6 @@
 -- Write a script that creates the database hbtn_0d_2 and the user user_0d_2
+-- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
+-- The user_0d_2 password should be set to user_0d_2_pwd
 
 CREATE TABLE IF NOT EXISTS force_name (
        id INT,
