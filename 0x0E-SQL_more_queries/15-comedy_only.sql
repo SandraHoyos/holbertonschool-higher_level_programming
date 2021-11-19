492b626c003524566db70c7c8595828fd2194588
SandraHoyos <slhoyosr@gmail.com>
2021-11-18 22:49:09 -0500

    comedy only

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,12 @@
+-- Import the database dump from hbtn_0d_tvshows
+-- that lists all Comedy shows in the database hbtn_0d_tvshows
+-- Results must be sorted in ascending order by the show title
+
+ SELECT tv_shows.title
+     FROM tv_shows
+INNER JOIN tv_show_genres
+        ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+      	ON tv_show_genres.genre_id = tv_genres.id
+     WHERE tv_genres.name = "comedy"
+   ORDER BY tv_shows.title ASC;
