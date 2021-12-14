9d002f87f3a6f85477a2e63d15d3a89990c40be9
SandraHoyos <slhoyosr@gmail.com>
2021-12-13 22:01:46 -0500

    Fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,12 +4,12 @@ let squareIndicator = '';
 if (!inputSize) {
  console.log('Missing size');
 } else {
-     for (let i = 0; i < inputSize; i++) {
-       for (let j = 0; j < inputSize; j++) {
-         squareIndicator += 'X';
-       }
-    if (i !== inputSize - 1) {
-     squareIndicator += '\n';
+  for (let i = 0; i < inputSize; i++) {
+    for (let j = 0; j < inputSize; j++) {
+      squareIndicator += 'X';
+    }
+  if (i !== inputSize - 1) {
+   squareIndicator += '\n';
   }
  }
  console.log(squareIndicator);
