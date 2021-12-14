f7e078183c16435931ca53162059934aa425391e
SandraHoyos <slhoyosr@gmail.com>
2021-12-13 22:59:24 -0500

    second

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 if (process.argv.length < 4) {
   console.log(0);
 } else {
-  let number = process.argv.length;
+  let number = process.argv;
   number.sort();
   console.log(parseInt(number[number.length-2]));
 }
