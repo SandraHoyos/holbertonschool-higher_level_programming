ebf7189f284e7e0290c13c46dcd8efac96653731
SandraHoyos <slhoyosr@gmail.com>
2021-12-13 22:07:28 -0500

    Fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-const inputSize = parseInt(process.argv[2]);
-let squareIndicator = '';
-if (!inputSize) {
- console.log('Missing size');
+if (!process.argv[2] || !parseInt(process.argv[2])) {
+  console.log('Missing size');
 } else {
   for (let i = 0; i < process.argv[2]; i++) {
     console.log('X'.repeat(process.argv[2]));
