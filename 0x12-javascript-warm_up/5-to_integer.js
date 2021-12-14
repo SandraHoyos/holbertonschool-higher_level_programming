d3f9a472d5f6108b9611c82949e60355919d18f1
SandraHoyos <slhoyosr@gmail.com>
2021-12-13 19:39:04 -0500

    to integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,6 +2,6 @@
 const number = parseInt(process.argv[2]);
 if (!number) {
   console.log('Not a number');
-}else {
-    console.log('My number: '+ number);
+} else {
+  console.log('My number: '+ number);
 }
