900ae8ea1d820b52d06e47e69e55ed86366b9c6a
SandraHoyos <slhoyosr@gmail.com>
2021-12-13 22:26:40 -0500

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 function add (a, b) {
- let a = parseInt(process.argv[2]);
- let b = parseInt(process.argv[3]);
+ a = parseInt(process.argv[2]);
+ b = parseInt(process.argv[3]);
  const result = a + b;
  console.log(a + b);
 }
