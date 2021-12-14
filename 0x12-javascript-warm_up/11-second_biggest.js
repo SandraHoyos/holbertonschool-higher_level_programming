c0a612f830952a04d875570cba9a4ac510fc2642
SandraHoyos <slhoyosr@gmail.com>
2021-12-13 23:39:00 -0500

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const max = process.argv;
-if (!max[3]) {
-  console.log(0);
-} else if (max[2] && max[3]) {
-  max.sort();
-  const number = max.length;
-  console.log(parseInt(max[number - 2]));
+let num = 0;
+let args = process.argv.slice(2);
+if (args.length > 1) {
+  args.sort();
+  num = args[args.length - 2];
 }
+console.log(num);
