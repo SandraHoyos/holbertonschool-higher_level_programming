0e30a61d7f15c73baebfa9b50b5e99259fff9fba
SandraHoyos <slhoyosr@gmail.com>
2021-12-13 21:46:25 -0500

    Fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,12 +2,12 @@
 const inputSize = parseInt(process.argv[2]);
 let squareIndicator = '';
 if (!inputSize) {
-  console.log('Missing size');
+ console.log('Missing size');
 } else {
-  for (let i = 0; i < inputSize; i++) {
-    for (let j = 0; j < inputSize; j++) {
-      squareIndicator += 'X';
-    }
+     for (let i = 0; i < inputSize; i++) {
+       for (let j = 0; j < inputSize; j++) {
+         squareIndicator += 'X';
+       }
     if (i !== inputSize - 1) {
      squareIndicator += '\n';
   }
