a78188f1663ae26016fb3dc299817848ac2e0345
SandraHoyos <slhoyosr@gmail.com>
2021-12-13 21:41:25 -0500

    Fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
 const inputSize = parseInt(process.argv[2]);
 let squareIndicator = '';
-
-if (process.argv.length < 3 || isNaN(inputSize)) {
+if (!inputSize) {
   console.log('Missing size');
-}else {
- for (let i = 0; i < inputSize; i++) {
-   for (let j = 0; j < inputSize; j++) {
-     squareIndicator += 'X';
-   }
-   if (i !== inputSize - 1) {
-    squareIndicator += '\n';
-   }
+} else {
+  for (let i = 0; i < inputSize; i++) {
+    for (let j = 0; j < inputSize; j++) {
+      squareIndicator += 'X';
+    }
+    if (i !== inputSize - 1) {
+     squareIndicator += '\n';
+  }
  }
  console.log(squareIndicator);
 }
