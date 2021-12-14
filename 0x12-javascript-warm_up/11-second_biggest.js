b0a75a0a6df5ee96bfdbef01cc5f719ebe93abc1
SandraHoyos <slhoyosr@gmail.com>
2021-12-13 23:23:56 -0500

    second1

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ if (process.argv.length < 4) {
   console.log(0);
 } else {
   let number = process.argv;
-  number.sort();
   number.slice(2);
+  number.sort();
   console.log(parseInt(number[number.length - 2]));
 }
