ce62ceec4767e0749b5a8cf99eb535aed5055da7
Sandra Hoyos <slhoyosr@gmail.com>
2021-12-14 21:53:59 -0500

    rotate

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+class Rectangle {
+    constructor (w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+    print () {
+      let i = 0;
+      for (i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+    rotate () {
+      let w = this.width;
+      let h = this.height;
+      this.height = w;
+      this.width = h;
+    }
+    double () {
+      this.height = this.height * 2;
+      this.width = this.width * 2;
+    }
+  }
+  module.exports = Rectangle;
