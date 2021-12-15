966ddfb4e5053bfca0f43a936b9f675f02b9e159
Sandra Hoyos <slhoyosr@gmail.com>
2021-12-14 21:49:51 -0500

    Fix 14

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || !w || !h) {
@@ -7,9 +8,10 @@ class Rectangle {
     this.width = w;
     this.height = h;
   }
+
   print () {
     for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width) );
+      console.log('X'.repeat(this.width));
     }
   }
 }
