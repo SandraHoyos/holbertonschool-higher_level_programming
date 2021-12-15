464ed92aae33ae5ac60601c8aacf890e6fd1cc0a
Sandra Hoyos <slhoyosr@gmail.com>
2021-12-14 22:34:42 -0500

    Update 11-second_biggest.js
    fix semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 let num = 0;
 const args = process.argv.slice(2);
 if (args.length > 1) {
-args.sort();
-num = args[args.length - 2];
+  args.sort();
+  num = args[args.length - 2];
 }
 console.log(parseInt(num));
