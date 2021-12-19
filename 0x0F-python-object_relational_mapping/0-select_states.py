6acb8731dba93527882aa76a1efb8b16713644d0
Sandra Hoyos <slhoyosr@gmail.com>
2021-12-18 19:07:37 -0500

    first commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+script to list all states from db hbtn_0e_usa
+takes 3 args username, mysql passwd, db name
+"""
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    c = db.cursor()
+    c.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+    lst = c.fetchall()
+    for r in lst:
+        print(r)
+    c.close()
+    db.close()
