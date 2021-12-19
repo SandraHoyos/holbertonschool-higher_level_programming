72eed5ffcb481c546c4ebd8904dcfdd0794231ab
Sandra Hoyos <slhoyosr@gmail.com>
2021-12-18 20:45:43 -0500

    Create model_state.py

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/phyton3
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+Base = declarative_base()
+
+class State(Base):
+    """Class State inherits from Base"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
