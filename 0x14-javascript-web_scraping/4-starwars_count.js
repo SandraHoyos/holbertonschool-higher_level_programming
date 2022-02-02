1729d92d77872672de23c635360bc57b8ec406da
SandraHoyos <slhoyosr@gmail.com>
2022-02-01 21:49:01 -0500

    update

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,3 +3,17 @@
 const fs = require('fs');
 const request = require('request');
 request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
+#!/usr/bin/node
+// makes get request for SW movie id
+const request = require('request');
+const find = '/18/';
+request(process.argv[2], function (error, response, body) {
+  if (error) throw new Error(error);
+  let num = 0;
+  for (const film of JSON.parse(body).results) {
+    for (const character of film.characters) {
+      num += (character.includes(find) ? 1 : 0);
+    }
+  }
+  console.log(num);
+});
