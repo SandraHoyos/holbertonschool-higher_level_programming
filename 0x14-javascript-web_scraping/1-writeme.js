11086d64163a5ffeab39110df845a07245cded7d
SandraHoyos <slhoyosr@gmail.com>
2022-02-01 21:34:26 -0500

    update file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-
-/* writes a string to a file */
+// reads file passed as arg
 const fs = require('fs');
-
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err, data) {
-  if (err) {
-    console.log(err);
-  }
+fs.writeFile(process.argv[2], process.argv[3], (error) => {
+  error && console.log(error);
 });
